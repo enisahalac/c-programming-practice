9d6acb1526e42f8a6a648c595710348a7b6c63cd
enisahalac <halacenisa@gmail.com>
2025-12-27 18:22:18 +0100

    palindrome

---
+++ arrays/palindrome.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int palindrome(int array[], int n) {
+    int i;
+    int count = 1;
+    int existence = 0;
+
+    for (i = 0; i < n; i++) {
+        if (array[i] == array[n-count]) {
+            existence = 1;
+            count++;
+        }
+        else {
+            existence = 0;
+            break;
+        }
+    }
+
+    return existence;
+}
+
+int main () {
+    int array[] = {1, 2, 3, 4, 3, 2, 1};
+    int n = 7;
+
+    int x = palindrome(array, n);
+
+    printf("%s", x ? "YES" : "NO");
+
+    return 0;
+}
