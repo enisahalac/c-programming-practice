75d53755e09562a2a1534f8690b8cdf0c70c8d80
enisahalac <halacenisa@gmail.com>
2025-12-29 05:44:02 +0100

    sum of odd numbers from 1 to n

---
+++ recursion/sum_of_odd_numbers_from_1_to _n.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int sumOddNumbers(int n) {
+    if (n == 0) {
+        return 0;
+    }
+
+    if (n % 2 == 1) {
+        return n + sumOddNumbers(n-1);
+    }
+    else {
+        return sumOddNumbers(n-1);
+    }
+}
+
+int main () {
+    int n = 1;
+
+    int a = sumOddNumbers(n);
+
+    printf("%d", a);
+
+
+    return 0;
+}
