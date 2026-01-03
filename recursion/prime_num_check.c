e0bb03400295ff17f19a3d864f620512065cc113
enisahalac <halacenisa@gmail.com>
2026-01-03 18:56:26 +0100

    prime num check

---
+++ recursion/prime_num_check.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int isDivisible(int n, int i) {
+    if (i >= n) {
+        return 0;
+    }
+
+    if (n % i == 0) {
+        return 1;
+    }
+
+    return isDivisible(n, i + 1);
+}
+
+int primeNumRec(int n) {
+    if (n <= 1) {
+        return 0;
+    }
+
+    if (isDivisible(n, 2)) {
+        return 0;
+    }
+
+    return 1;
+}
+
+int main () {
+    int n;
+    int i;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (primeNumRec(n)) {
+        printf("Prime");
+    }
+    else {
+        printf("Not prime");
+    }
+
+    return 0;
+}
