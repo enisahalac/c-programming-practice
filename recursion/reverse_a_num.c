c65a628c2db5afd444871d09adf56760706b6048
enisahalac <halacenisa@gmail.com>
2026-01-03 22:11:23 +0100

    reverse a num

--- recursion/reverse_a_num.c
+++ recursion/reverse_a_num.c
@@ -1,28 +1,31 @@
 #include <stdio.h>
 
-void reverseNumRec(int n) {
+int reverseNumRec(int n, int a) {
     if (n == 0) {
-        return;
+        return a;
     }
 
-    printf("%d", n % 10);
-    reverseNumRec(n / 10);
+    return reverseNumRec(n / 10, a * 10 + n % 10);
 }
 
-void reverseNum(int n) {
+int reverseNum(int n, int a) {
     while (n > 0) {
-        printf("%d", n % 10);
+        a = a * 10 + n % 10;
         n = n / 10;
     }
+
+    return a;
 }
+
 
 int main () {
     int n;
+    int a = 0;
 
     printf("Enter a num: ");
     scanf("%d", &n);
 
-    reverseNumRec(n);
+    printf("%d", reverseNum(n, a));
 
     return 0;
 }
