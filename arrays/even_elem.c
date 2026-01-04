db083b7977c5674c9d502ecaeddc94c9ed954d24
enisahalac <halacenisa@gmail.com>
2026-01-04 13:40:01 +0100

    product of elem

---
+++ arrays/even_elem.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main () {
+    
+    int niz[7] = {2, 4, 5, 6, 8, 1, 2};
+    
+    int brojac_parnih = 0; 
+    
+    int i;
+    
+    for (i = 0; i < 7; i++) {
+        if (niz[i] % 2 == 0) {
+            brojac_parnih += 1; // 1, 
+        }
+    }
+    
+    printf("%d", brojac_parnih);
+    
+    return 0;
+}
