2206802cdbabded2c5fe6d58e659964695c938c6
enisahalac <halacenisa@gmail.com>
2026-01-12 19:33:05 +0100

    mv even num

---
+++ loops/mv_even_num.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int x, digit;
+    int sum = 0;
+    int temp = 0;
+    
+    printf("Enter a num: ");
+    scanf("%d", &x);
+    
+    do {
+       digit = abs(x % 10);
+       if (digit % 2 == 0) {
+           sum += digit;
+           temp++;
+        }
+       x = x / 10;
+    } while (x != 0);
+    
+    if (temp > 0) {
+        printf("The mean value for even numbers is: %.2f", (double)sum / temp);
+    } else {
+        printf("There is no even digits.");
+    }
+
+    return 0;
+}
