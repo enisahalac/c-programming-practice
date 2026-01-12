5a689cca1c826851e812ad812ee4495d8e59ee70
enisahalac <halacenisa@gmail.com>
2026-01-12 18:48:57 +0100

    even num interval

---
+++ loops/even_num_interval.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main () {
+    int n = 5;
+    int i;
+    
+    for (i = n; i <= n*n; i++) {
+        if (i % 2 == 0) {
+            printf("%d ", i);
+        }
+    }
+    
+    return 0;
+}
