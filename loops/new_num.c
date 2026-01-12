4b5aa3e0883b029399c17c0c63a9e31921808235
enisahalac <halacenisa@gmail.com>
2026-01-12 19:31:02 +0100

    new num

---
+++ loops/new_num.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main () {
+    int x, y = 0, z;
+    
+    printf("Enter a num: ");
+    scanf("%d", &x);
+    
+    do {
+        z = x % 10;
+        y = y * 10 + z;
+        x = x / 10;
+    } while (x != 0);
+    
+    printf("New number: %d ", y);
+    
+    return 0;
+}
