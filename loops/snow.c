073483d66bda92783ce943dc443b44172a2749aa
enisahalac <halacenisa@gmail.com>
2026-01-12 23:41:00 +0100

    snow

---
+++ loops/snow.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    
+    int i, j;
+    
+    for (i = 0; i < n; i++) {
+        if (i % 2 == 0) {
+            printf(" ");
+        }
+        for (j = 0; j < n; j++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
