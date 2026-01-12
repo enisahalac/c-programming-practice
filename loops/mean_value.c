1f00eb8dc4ed68c488020d803c1c8ffdd9cec99d
enisahalac <halacenisa@gmail.com>
2026-01-12 19:31:58 +0100

    mean value

---
+++ loops/mean_value.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int x, digit;
+    int sum = 0;
+    int temp = 0;
+    
+    printf("Enter a num: ");
+    scanf("%d", &x);
+    
+    do {
+       digit = x % 10;
+       sum += digit;
+       temp++;
+       x = x / 10;
+    } while (x != 0);
+    
+    printf("The mean value is: %.2f", (double)sum / temp);
+
+    return 0;
+}
