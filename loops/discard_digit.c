14f9dff1f753ed3101c25f7c07551233ee4ac1e4
enisahalac <halacenisa@gmail.com>
2026-01-12 19:34:13 +0100

    discard digit

---
+++ loops/discard_digit.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int x, z;
+    
+    printf("Enter a num: ");
+    scanf("%d", &x);
+    
+    printf("Digit to discard: ");
+    scanf("%d", &z);
+    
+    int digit;
+    int revNum = 0;
+    
+    while (x != 0) {
+        digit = x % 10;
+        if (digit != z) {
+            revNum = revNum * 10 + digit;
+        }
+        x /= 10;
+    }
+    
+    int newNum = 0; 
+    
+    while (revNum != 0) {
+        digit = revNum % 10;
+        newNum = newNum * 10 + digit;
+        revNum /= 10;
+    }
+    
+    printf("%d", newNum);
+    
+    return 0;
+}
