b0891d5285f5bd7e3450b8c7568518e0ffd127e7
enisahalac <halacenisa@gmail.com>
2026-01-14 23:49:01 +0100

    letter Z

---
+++ loops and if-else/letter_Z.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+	int n;
+	int i, j;
+
+	do {
+		printf("Unesite broj n: ");
+		scanf("%d", &n);
+	} while (n <= 4);
+
+	for (i = 0; i < n; i++) {
+		for (j = 0; j < n; j++) {
+			if (i == 0 || i == n -1) {
+			printf("*");
+			} else if (j == n - 1 - i) {
+				printf("*");
+			} else {
+				printf(" ");
+			}
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
