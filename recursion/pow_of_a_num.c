f359095d1b94fa5b6a4dfed94e25702769a0be1c
enisahalac <halacenisa@gmail.com>
2026-01-16 01:40:31 +0100

    pow of a num

--- recursion/pow_of_a_num.c
+++ recursion/pow_of_a_num.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
-int powNumRecursion(int x, int n) {
+int powNumRec(int x, int n) {
     if (n == 0) {
         return 1;
     }
 
-    return  x * powNumRecursion(x, n-1);
+    return  x * powNumRec(x, n-1);
 }
 
 int main () {
     int x = 2;
     int n = 4;
 
-    int m = powNumRecursion(x, n);
+    int m = powNumRec(x, n);
 
     printf("%d", m);
 
